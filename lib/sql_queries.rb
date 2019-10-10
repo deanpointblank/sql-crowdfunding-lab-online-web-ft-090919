fb86009123014fecdf2763c66244d073da312ce3
Dean L Betty <deanlloyd64@gmail.com>
2019-10-10 17:32:20 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -52,7 +52,9 @@ end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
   <<-SQL
-  SELECT 
+  SELECT projects.name, SUM(pledges.amount)
+  AS total
+  FROM
   SQL
 end
 

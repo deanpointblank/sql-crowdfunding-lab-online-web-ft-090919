aa6ee50b570c90b3a528c4f578a0c16c24873528
Dean L Betty <deanlloyd64@gmail.com>
2019-10-10 17:52:21 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -54,7 +54,7 @@ def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_ca
   <<-SQL
   SELECT projects.category, pledges.amount
   FROM projects
-  JOIN LEFT pledges
+  JOIN pledges
   ON projects.id = pledges.project_id
   SQL
 end

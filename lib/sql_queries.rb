428f72ccc158fda0dea9ce9dd1bb7a9b06f1454c
Dean L Betty <deanlloyd64@gmail.com>
2019-10-10 17:27:11 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -40,7 +40,10 @@ end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_summed_amount
   <<-SQL
-  
+  SELECT users.name, SUM(pledges.amount)
+  FROM users
+  JOIN pledges
+  ON users.id = 
   SQL
 end
 

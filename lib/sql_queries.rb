5b323a08c2934ceee50876407aea6daf808e869f
Dean L Betty <deanlloyd64@gmail.com>
2019-10-10 17:50:56 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -63,3 +63,26 @@ end
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
   "Write your SQL query Here"
 end
+
+
+# CREATE TABLE projects (
+#   id INTEGER PRIMARY KEY,
+#   title TEXT,
+#   category TEXT,
+#   funding_goal INTEGER,
+#   start_date TEXT,
+#   end_date TEXT
+# );
+
+# CREATE TABLE users (
+#   id INTEGER PRIMARY KEY,
+#   name TEXT,
+#   age INTEGER
+# );
+
+# CREATE TABLE pledges (
+#   id INTEGER PRIMARY KEY,
+#   amount INTEGER,
+#   user_id INTEGER,
+#   project_id INTEGER
+# );

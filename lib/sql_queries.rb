3e0158c2af484b315f2646647c0037861bb022fe
Dean L Betty <deanlloyd64@gmail.com>
2019-10-10 17:35:50 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -56,7 +56,7 @@ def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_ca
   AS total
   FROM projects
   JOIN pledges
-  ON project.id = pledges.project_id
+  ON projects.id = pledges.project_id
   WHERE projects.category = "Music"
   SQL
 end
